23753ec22c9626bbb23433c7293a040f419d555a
Nikita Nazarov <nnazarov55@gmail.com>
2023-10-25 15:18:17 +0300

    ref repo

--- lab-5/script1.js
+++ lab-5/script1.js
@@ -26,3 +26,31 @@ images[1].addEventListener('click', function () {
     this.setAttribute('data-blurred', true);
   }, 1200);
 })
+
+images[2].addEventListener('click', function () {
+  const isBlurred = this.getAttribute('data-blurred') === 'true';
+  this.setAttribute('data-blurred', !isBlurred);
+  if (isBlurred) {
+    this.src = 'нерозмите2.jpg'; 
+  } else {
+    this.src = 'розмите2.jpg';
+  }
+  setTimeout(() => {
+    this.src = 'розмите2.jpg'; 
+    this.setAttribute('data-blurred', true);
+  }, 1200);
+})
+
+images[3].addEventListener('click', function () {
+  const isBlurred = this.getAttribute('data-blurred') === 'true';
+  this.setAttribute('data-blurred', !isBlurred);
+  if (isBlurred) {
+    this.src = 'нерозмите2.jpg'; 
+  } else {
+    this.src = 'розмите2.jpg';
+  }
+  setTimeout(() => {
+    this.src = 'розмите2.jpg'; 
+    this.setAttribute('data-blurred', true);
+  }, 1200);
+})
---
+++ lab-5/script2.js
@@ -0,0 +1,27 @@
+const image = document.querySelectorAll('.image');
+
+
+image[0].addEventListener('mouseenter', function () {
+  this.src = 'нерозмите1.jpg'; 
+});
+image[0].addEventListener('mouseleave', function () {
+  this.src = 'розмите1.jpg'; 
+});
+image[1].addEventListener('mouseenter', function () {
+  this.src = 'нерозмите2.jpg'; 
+});
+image[1].addEventListener('mouseleave', function () {
+  this.src = 'розмите2.jpg';
+});
+image[2].addEventListener('mouseenter', function () {
+  this.src = 'нерозмите2.jpg'; 
+});
+image[2].addEventListener('mouseleave', function () {
+  this.src = 'розмите2.jpg';
+});
+image[3].addEventListener('mouseenter', function () {
+  this.src = 'нерозмите2.jpg'; 
+});
+image[3].addEventListener('mouseleave', function () {
+  this.src = 'розмите2.jpg';
+});
